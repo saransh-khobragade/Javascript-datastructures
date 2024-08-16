8bebffd139656c9dfbba7516d96f8f25277d2bf7
saransh-khobragade <saransh98@gmail.com>
2024-08-16 10:50:57 +0530

    done

---
+++ leetcode/1. Two Sum.js
@@ -0,0 +1,15 @@
+
+//https://leetcode.com/problems/two-sum/description/
+/**
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1] */
+
+
+const nums = [2,7,11,15]
+target = 9
+
+var twoSum = function(nums, target) {
+    console.log(nums)
+};
+
+console.log(twoSum(nums,target))
